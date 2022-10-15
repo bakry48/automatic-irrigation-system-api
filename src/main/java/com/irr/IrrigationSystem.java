a11d80fef756e2ded2ddb5c6ffce0eae84e48210
bakry84 <115186806+bakry84@users.noreply.github.com>
2022-10-15 23:17:36 +0200

    classes name update

---
+++ src/main/java/com/irr/IrrigationSystem.java
@@ -0,0 +1,18 @@
+package com.irr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+
+@SpringBootApplication
+@EnableScheduling
+public class IrrigationSystem {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(IrrigationSystem.class, args);
+	}
+
+}
